6c2b205df47da782975d6a226e48210068a9b7af
JoyceJosephtwenty20 <joyce.j106s13@gmail.com>
2020-07-21 11:18:27 -0400

    firstcommit

---
+++ src/main/java/com/example/demo/SpringBoot301V2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot301V2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot301V2Application.class, args);
+    }
+
+}
